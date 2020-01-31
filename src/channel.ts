74c1d1af4652b565c568cefcbf65a3b50ff30e82
Liam Fitzgerald <liam@urizen.co>
2020-01-31 14:47:26 +1000

    Broke out channel interface into seperate package

---
+++ src/channel.ts
@@ -0,0 +1,261 @@
+// import { request, Config } from "./util";
+import * as EventSource from "eventsource";
+import * as http from 'http';
+
+export interface Config {
+  port: number;
+  delay: number;
+}
+interface HttpResponse {
+  req: http.ClientRequest;
+  res: http.IncomingMessage;
+  data: string;
+}
+
+export function request(
+  url: string,
+  options: http.ClientRequestArgs,
+  body?: string
+) {
+  return new Promise<HttpResponse>((resolve, reject) => {
+    const req = http.request(url, options, res => {
+      let data = "";
+      res.on("data", chunk => {
+        data += chunk;
+      });
+      res.on("end", () => {
+        resolve({ req, res, data });
+      });
+      res.on("error", e => {
+        reject(e);
+      });
+    });
+    if (body) {
+      req.write(body);
+    }
+    req.end();
+  });
+}
+
+export function wait(time: number) {
+  return new Promise((resolve) =>
+    setTimeout(resolve, time)
+  )
+}
+
+export default class Channel {
+  uid: string;
+  requestId: number;
+  eventSource: EventSource | null;
+  lastEventId: number;
+  lastAcknowledgedEventId: number;
+  outstandingPokes: Map<number, any>;
+  outstandingSubscriptions: Map<number, any>;
+
+  constructor(
+    private cookies: string[],
+    private url: string,
+    private config: Config
+  ) {
+    //  unique identifier: current time and random number
+    //
+    this.uid =
+      new Date().getTime().toString() +
+      "-" +
+      Math.random()
+        .toString(16)
+        .slice(-6);
+
+    this.requestId = 1;
+
+    //  the currently connected EventSource
+    //
+    this.eventSource = null;
+
+    //  the id of the last EventSource event we received
+    //
+    this.lastEventId = 0;
+
+    //  this last event id acknowledgment sent to the server
+    //
+    this.lastAcknowledgedEventId = 0;
+
+    //  a registry of requestId to successFunc/failureFunc
+    //
+    //    These functions are registered during a +poke and are executed
+    //    in the onServerEvent()/onServerError() callbacks. Only one of
+    //    the functions will be called, and the outstanding poke will be
+    //    removed after calling the success or failure function.
+    //
+
+    this.outstandingPokes = new Map();
+
+    //  a registry of requestId to subscription functions.
+    //
+    //    These functions are registered during a +subscribe and are
+    //    executed in the onServerEvent()/onServerError() callbacks. The
+    //    event function will be called whenever a new piece of data on this
+    //    subscription is available, which may be 0, 1, or many times. The
+    //    disconnect function may be called exactly once.
+    //
+    this.outstandingSubscriptions = new Map();
+  }
+
+  //  sends a poke to an app on an urbit ship
+  //
+  poke(
+    ship: string,
+    app: string,
+    mark: string,
+    json: any,
+    successFunc: any,
+    failureFunc: any
+  ) {
+    let id = this.nextId();
+    this.outstandingPokes.set(id, {
+      success: successFunc,
+      fail: failureFunc
+    });
+
+    this.sendJSONToChannel({
+      id,
+      action: "poke",
+      ship,
+      app,
+      mark,
+      json
+    });
+  }
+
+  //  subscribes to a path on an specific app and ship.
+  //
+  //    Returns a subscription id, which is the same as the same internal id
+  //    passed to your Urbit.
+  subscribe(
+    ship: string,
+    app: string,
+    path: string,
+    connectionErrFunc = (e: any) => {},
+    eventFunc = (e: any) => {},
+    quitFunc = (e: any) => {}
+  ) {
+    let id = this.nextId();
+    this.outstandingSubscriptions.set(id, {
+      err: connectionErrFunc,
+      event: eventFunc,
+      quit: quitFunc
+    });
+
+    this.sendJSONToChannel({
+      id,
+      action: "subscribe",
+      ship,
+      app,
+      path
+    });
+
+    return id;
+  }
+
+  //  unsubscribe to a specific subscription
+  //
+  unsubscribe(subscription: string) {
+    let id = this.nextId();
+    this.sendJSONToChannel({
+      id,
+      action: "unsubscribe",
+      subscription
+    });
+  }
+
+  //  sends a JSON command command to the server.
+  //
+  sendJSONToChannel(j: unknown) {
+    let body: any;
+    if (this.lastEventId == this.lastAcknowledgedEventId) {
+      body = JSON.stringify([j]);
+    } else {
+      //  we add an acknowledgment to clear the server side queue
+      //
+      //    The server side puts messages it sends us in a queue until we
+      //    acknowledge that we received it.
+      //
+      body = JSON.stringify([
+        { action: "ack", "event-id": this.lastEventId },
+        j
+      ]);
+
+      this.lastEventId = this.lastAcknowledgedEventId;
+    }
+    return request(
+      this.channelURL(),
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Content-Length": body.length,
+          Cookie: this.cookies
+        },
+        port: this.config.port
+      },
+      body
+    ).then(r => {
+      return this.connectIfDisconnected();
+    });
+  }
+
+  //  connects to the EventSource if we are not currently connected
+  //
+  connectIfDisconnected() {
+    if (this.eventSource) {
+      return;
+    }
+
+    const eventSource = new EventSource(this.channelURL(), {
+      headers: { Cookie: this.cookies[0], Connection: "keep-alive" },
+      withCredentials: true
+    });
+    eventSource.onmessage = (e: MessageEvent) => {
+      this.lastEventId = parseInt(e.lastEventId, 10);
+
+      let obj = JSON.parse(e.data);
+      if (obj.response == "poke") {
+        let funcs = this.outstandingPokes.get(obj.id);
+        if (obj.hasOwnProperty("ok")) {
+          funcs["success"]();
+        } else if (obj.hasOwnProperty("err") && funcs) {
+          funcs["fail"](obj.err);
+        } else {
+        }
+        this.outstandingPokes.delete(obj.id);
+      } else if (obj.response == "subscribe") {
+        //  on a response to a subscribe, we only notify the caller on err
+        //
+        let funcs = this.outstandingSubscriptions.get(obj.id);
+        if (obj.hasOwnProperty("err")) {
+          funcs["err"](obj.err);
+          this.outstandingSubscriptions.delete(obj.id);
+        }
+      } else if (obj.response == "diff") {
+        let funcs = this.outstandingSubscriptions.get(obj.id);
+        funcs["event"](obj.json);
+      } else if (obj.response == "quit") {
+        let funcs = this.outstandingSubscriptions.get(obj.id);
+        funcs["quit"](obj);
+        this.outstandingSubscriptions.delete(obj.id);
+      } else {
+      }
+    };
+
+    eventSource.onerror = e => {};
+    this.eventSource = eventSource;
+  }
+
+  channelURL() {
+    return this.url + ":" + this.config.port + "/~/channel/" + this.uid;
+  }
+
+  nextId() {
+    return this.requestId++;
+  }
+}
